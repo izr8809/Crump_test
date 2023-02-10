681458775c4c682af5e526d93641b1227789a456
izr8809 <izr8809@gmail.com>
2023-02-10 14:29:37 +0900

    !

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')                                        // core nodejs 모듈 중 하나, 파일 경로 설정할 때 사용
 const HtmlWebpackPlugin = require('html-webpack-plugin')            // core nodejs 모듈 중 하나, 파일 경로 설정할 때 사용
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')           // index.html 파일을 dist 폴더에 index_bundle.js 파일과 함께 자동으로 생성, 우리는 그냥 시작만 하고싶지 귀찮게 index.html 파일까지 만들고 싶지 않다.!!
+const webpack = require('webpack')
 
 module.exports = {       
     mode : 'development',                              // moduel export (옛날 방식..)
@@ -58,6 +59,9 @@ module.exports = {
         new MiniCssExtractPlugin({
           filename: "[name].css",
           chunkFilename: "[id].css"
+        }),
+        new webpack.DefinePlugin({
+          process : { env: {}}
         })
     ]
 }
